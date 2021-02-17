aae42a040c476e91b3b14fd7b10931348ba17c53
gouravsh4225 <gouravsh4225@gmail.com>
2021-02-17 23:33:23 +0530

    complete desktop view and add script file

---
+++ faq-accordion-card-main/scripts/main.js
@@ -0,0 +1,8 @@
+const questionListElement  = document.querySelectorAll('.question-item');
+questionListElement.forEach((question)=>{
+    question.addEventListener('click',(item)=>{
+        if(item.parentNode.classList.contains('d-none')) {
+            
+        }
+    })
+})
